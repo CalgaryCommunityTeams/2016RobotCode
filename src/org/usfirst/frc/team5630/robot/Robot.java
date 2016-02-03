5493043a815fcbffa467fb6d6798a77b21d1b274
Charlie-Zheng <charlie.x.3000@gmail.com>
2016-02-02 18:41:59 -0700

    I added the encoder for Flywheel. Controls are same as before.

--- src/org/usfirst/frc/team5630/robot/Robot.java
+++ src/org/usfirst/frc/team5630/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5630.robot;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -15,16 +16,21 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  */
 public class Robot extends IterativeRobot {
 	RobotDrive myRobot;
-	Talon flywheel;
+	//Talon flywheel;
 	Talon intake;
 	Talon arm;
 	Joystick stick;
-	double intakeSpeed;
+	double intakeSpeed;	
+	double flySpeed;
 	double armSpeed;
 	int autoLoopCounter;
+	int flyToggle;
+	int direction;
+	boolean directionToggle, directionToggleLast;
 	boolean flywheelRunLast;
 	boolean runFlywheel;
-	int counter;
+	CANTalon flyWheel;
+
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -32,11 +38,20 @@ public class Robot extends IterativeRobot {
 	 */
 	public void robotInit() {
 		myRobot = new RobotDrive(0, 1, 2, 3);
-		flywheel = new Talon(4);
+		//flywheel = new Talon(4);
 		intake = new Talon(5);
 		arm = new Talon(6);
 		stick = new Joystick(0);
-
+		flyWheel = new CANTalon(0); // Initialize the CanTalonSRX on device 1.
+	    flyWheel.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+	    flyWheel.changeControlMode(CANTalon.TalonControlMode.Speed);
+	    flyWheel.reverseSensor(false);
+	    flyWheel.configNominalOutputVoltage(+0.0f, -0.0f);
+	    flyWheel.configPeakOutputVoltage(+12.0f, -0.0f);
+	    flyWheel.setProfile(0);
+	    flyWheel.setPID(0.22, 0.0, 0.0);
+	    flyWheel.setF(0.1097);
+	    //Basically copy-pasta'd the code from an example code.
 	}
 
 	/**
@@ -65,7 +80,10 @@ public class Robot extends IterativeRobot {
 	 * mode
 	 */
 	public void teleopInit() {
-		counter = 0;
+		flyToggle = 0;
+		flySpeed = 0.8;
+		direction = 1;
+		flywheelRunLast=false;
 	}
 
 	/**
@@ -73,32 +91,57 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 		runFlywheel = stick.getRawButton(1);// Set this to be the flywheel input (Bool)
-		
+		directionToggle = stick.getRawButton(8);
 		armSpeed = stick.getRawAxis(3) - stick.getRawAxis(2);// Set this to be the armSpeed input (double)
 		
-		intakeSpeed = stick.getRawAxis(5) / 2; // Set this to be the intakeSpeed input (double)
-		//This is to reduce the speed of the intake motor
+		if (stick.getRawButton(5) != stick.getRawButton(6))
+		{
+			if (stick.getRawButton(5) == true)
+				intakeSpeed = 0.5;
+			else
+				intakeSpeed = -1;
+		}else
+			intakeSpeed = 0;
+		// Set this to be the intakeSpeed input (double)
+		//This is to reduce the speed of the intake motor{
 	
-		if (flywheelRunLast != runFlywheel) { // Enables Togglling
+		{
+			if (stick.getRawButton(3) == true && stick.getRawButton(2) == false && stick.getRawButton(4) == false)
+				flySpeed = 4000;//The motor doesn't reach 4000 RPM
+			else if (stick.getRawButton(4) == true && stick.getRawButton(2) == false && stick.getRawButton(3) == false)
+				flySpeed = 1200; //Maxs out at 1200 RPM
+			else if (stick.getRawButton(2) == true && stick.getRawButton(3) == false && stick.getRawButton(4) == false)
+				flySpeed = 600;
+		}
+		
+		
+		if (flywheelRunLast != runFlywheel) 
+		{ // Enables Togglling
 			flywheelRunLast = runFlywheel;
 			if (flywheelRunLast == true)
-				counter = (counter + 1) % 2; // If the counter is 0, it becomes 1, if the counter is 1, it becomes 0 -C. Zheng 2016-1-28
+				flyToggle = (flyToggle + 1) % 2; // If the counter is 0, it becomes 1, if the counter is 1, it becomes 0 -C. Zheng 2016-1-28
 		}
-
-		if (counter == 1) {
-			flywheel.set(-1.0); // flywheel Speed is changed here 
+		if (directionToggleLast != directionToggle) 
+		{
+			directionToggleLast = directionToggle;
+			if (directionToggleLast == true)
+				direction = -direction; // If the counter is 0, it becomes 1, if the counter is 1, it becomes 0 -C. Zheng 2016-1-28
+		}
+		if (flyToggle == 1) 
+		{
+			flyWheel.set(flySpeed); // flywheel Speed is changed here 
 			//This is 1.0 because we tested it at this value and nothing f*cked up -C. Zheng 2016-1-28
 			//Changed to -1.0 because it was reversed. -C. Zheng 2016-1-29
-			} else {
-			flywheel.set(0.0); // flywheel stops
-		}
+			//Now sets the Talon SRX to the set speed
+		} else 
+				flyWheel.set(0); // flywheel stops
 		//intakeSpeed = (intakeSpeed / 2) + 0.5;
 		//armSpeed = (armSpeed / 2) + 0.5;
 		//Commented because these were based off the Trigger buttons going from -1 to 1, with -1 being the "not pressed" position. This is not the case. The triggers go from 0 to 1.
 		intake.set(intakeSpeed);
-		arm.set(armSpeed);
-		myRobot.arcadeDrive(stick.getY(),-stick.getX());
-		;
+		arm.set(armSpeed/2);
+		myRobot.arcadeDrive(direction*stick.getY(),-stick.getX());
+
 	}
 
 	/**
