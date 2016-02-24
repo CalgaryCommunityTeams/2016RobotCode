48349e9a90ce0664ad7cb54ce126ed09f5616a1a
Charlie-Zheng <charlie.x.3000@gmail.com>
2016-02-23 20:54:45 -0700

    Changed arm speed to a sqrt of the joystick input.

--- src/org/usfirst/frc/team5630/robot/Robot.java
+++ src/org/usfirst/frc/team5630/robot/Robot.java
@@ -19,36 +19,55 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 public class Robot extends IterativeRobot {
 	RobotDrive robotDrive1;
 	Joystick joystickInput1;
-	Talon intakeDriver, armDriver;
+	Talon intakeDriver;
 	DigitalInput intakeBumper;
 	final int maxIntakeTime = 1500, extraIntake = 7;
-	double flySpeed, armSpeed, intakeSpeed = 0.5;
+	double flySpeed, armTargetPosition, armSpeed, intakeSpeed = 0.5;
 	int autoLoopCounter, flywheelEnable, direction, autoIntakeTimer;
 	boolean buttonStartLast, buttonALast, autoIntakeEnable, buttonBackLast = false;
 	boolean buttonA, buttonB, buttonX, buttonY, buttonLB, buttonRB, buttonBack, buttonStart;
+	int buttonPOV;
 	boolean autoEnableFlywheel = false; // Set options for features
 	CameraServer Camera;
-	CANTalon flyWheel;
+	CANTalon flyWheel, arm;
+	double reverseLimit = 0.35;
+	double forwardLimit = 0;
 
 	public void robotInit() {
-		// This function is run when the robot is first started up and should be used for any initialization code.
+		// This function is run when the robot is first started up and should be
+		// used for any initialization code.
 		robotDrive1 = new RobotDrive(0, 1, 2, 3);
-		//flywheelDriver = new Talon(4);
+		// flywheelDriver = new Talon(4);
 		intakeDriver = new Talon(5);
-		armDriver = new Talon(6);
+		// armDriver = new Talon(6);
 		joystickInput1 = new Joystick(0);
 		intakeBumper = new DigitalInput(0);
 		flyWheel = new CANTalon(0); // Initialize the CanTalonSRX on device
-		//1.
-		 flyWheel.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-		 flyWheel.changeControlMode(CANTalon.TalonControlMode.Speed);
-		 flyWheel.configEncoderCodesPerRev(1024);
-		 flyWheel.reverseSensor(false);
-		 flyWheel.configNominalOutputVoltage(+0.0f, -0.0f);
-		 flyWheel.configPeakOutputVoltage(+12.0f, -0.0f);
-		 flyWheel.setProfile(0);
-		 flyWheel.setPID(0.22, 0.0, 0.0);
-		 flyWheel.setF(0.1097);
+		// 1.
+		flyWheel.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		flyWheel.changeControlMode(CANTalon.TalonControlMode.Speed);
+		flyWheel.configEncoderCodesPerRev(1024);
+		flyWheel.reverseSensor(false);
+		flyWheel.configNominalOutputVoltage(+0.0f, -0.0f);
+		flyWheel.configPeakOutputVoltage(+12.0f, -0.0f);
+		flyWheel.setProfile(0);
+		flyWheel.setPID(0.22, 0.0, 0.0);
+		flyWheel.setF(0.1097);
+
+		arm = new CANTalon(1); // Initialize the CanTalonSRX on device
+		arm.setEncPosition(arm.getPulseWidthPosition() & 0xFFF);
+		arm.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		arm.configEncoderCodesPerRev(1024);
+		arm.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+		arm.reverseSensor(false);
+		arm.reverseOutput(true);
+		arm.configNominalOutputVoltage(+0.0f, -0.0f);
+		arm.configPeakOutputVoltage(+12.0f, -12.0f);
+		arm.setAllowableClosedLoopErr(0);
+		arm.setProfile(0);
+		arm.setPID(0.22, 0.0017, 0.00003);
+		arm.setReverseSoftLimit(reverseLimit);
+		arm.setForwardSoftLimit(forwardLimit);
 
 		// the camera name (ex "cam0") can be found through the roboRIO web
 		// interface
@@ -65,8 +84,9 @@ public class Robot extends IterativeRobot {
 		// This function is called periodically during autonomous
 	}
 
-	public void teleopInit() { 
-		// This function is called once each time the robot enters tele-operated mode
+	public void teleopInit() {
+		// This function is called once each time the robot enters tele-operated
+		// mode
 		flywheelEnable = 0;
 		flySpeed = 0.8;
 		direction = 1;
@@ -87,18 +107,19 @@ public class Robot extends IterativeRobot {
 		buttonRB = joystickInput1.getRawButton(6);
 		buttonBack = joystickInput1.getRawButton(7);
 		buttonStart = joystickInput1.getRawButton(8);
-
+		buttonPOV = joystickInput1.getPOV();
+		//armTargetPosition = armTargetPosition + (joystickInput1.getRawAxis(3) - joystickInput1.getRawAxis(2)) / 150;
 		armSpeed = joystickInput1.getRawAxis(3) - joystickInput1.getRawAxis(2);
-
 		if (autoIntakeEnable == true) {
 			intakeDriver.set(-intakeSpeed);
 			autoIntakeTimer++;
 			if (intakeBumper.get() == false && autoIntakeTimer < maxIntakeTime - extraIntake) {
 				// Bumper has been pressed
-				
-				// Set the autoIntakeTimer to maximum time (minus the extra run time to get ball off the ground)
+
+				// Set the autoIntakeTimer to maximum time (minus the extra run
+				// time to get ball off the ground)
 				autoIntakeTimer = maxIntakeTime - extraIntake;
-				
+
 				// Enable flywheel
 				if (autoEnableFlywheel)
 					flywheelEnable = 1;
@@ -112,7 +133,7 @@ public class Robot extends IterativeRobot {
 			autoIntakeEnable = true;
 			autoIntakeTimer = 0;
 		}
-		
+
 		if (buttonRB) {
 			intakeDriver.set(-intakeSpeed);
 		} else if (buttonLB) {
@@ -122,41 +143,42 @@ public class Robot extends IterativeRobot {
 		}
 
 		if (buttonX == true && buttonB == false && buttonY == false) {
-			flySpeed = 4000; //The motor doesn't reach 4000 RPM
-			//flySpeed = 1.0;
+			flySpeed = 4000; // The motor doesn't reach 4000 RPM
+			// flySpeed = 1.0;
 		} else if (buttonY == true && buttonB == false && buttonX == false) {
-			flySpeed = 1200; //Maxs out at 1200 RPM
-			//flySpeed = 0.8;
+			flySpeed = 1800; // Maxs out at 1200 RPM
+			// flySpeed = 0.8;
 		} else if (buttonB == true && buttonX == false && buttonY == false) {
-			flySpeed = 600;
-			//flySpeed = 0.6;
+			flySpeed = 1200;
+			// flySpeed = 0.6;
 		}
 
-		if (buttonALast != buttonA) { 
+		if (buttonALast != buttonA) {
 			// Enables Toggling of Flywheel
 			buttonALast = buttonA;
 			if (buttonALast == true)
-				flywheelEnable = (flywheelEnable + 1) % 2; // Toggles Flywheel between 0 and 1
+				flywheelEnable = (flywheelEnable + 1) % 2; // Toggles Flywheel
+															// between 0 and 1
 		}
-		
+
 		if (buttonStartLast != buttonStart) {
 			buttonStartLast = buttonStart;
 			if (buttonStartLast == true)
-				direction = -direction; 
+				direction = -direction;
 		}
-		
+
 		if (flywheelEnable == 1) {
 			flyWheel.set(flySpeed);
 		} else
 			flyWheel.set(0);
 
 		buttonBackLast = buttonBack;
-		armDriver.set(armSpeed / 2);
-		
+		//arm.set(armTargetPosition);
+		arm.set(armSpeed/Math.sqrt(Math.abs(armSpeed)));
 		robotDrive1.arcadeDrive(direction * joystickInput1.getRawAxis(1), -joystickInput1.getRawAxis(4));
 	}
 
-	public void testPeriodic() { 
+	public void testPeriodic() {
 		// This function is called periodically during test mode
 		LiveWindow.run();
 	}
